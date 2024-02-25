c6ec06530f3c648f457185d09b18a650a89d8ba1
Mizna Masood <miznamasood2007@gmail.com>
2024-02-25 11:09:09 -0800

    Chapter 4 & 5(half)

---
+++ app.js
@@ -0,0 +1,151 @@
+// Chap 4
+
+// Q1
+
+var firstName = "Jhon", lastName = "Doe" , age = "30";
+
+// Q2
+
+// Legal variable ____________
+
+  var userName = ""
+  var _totalAmount = ""
+   var  $price = ""
+   var  myVariable = ""
+     var   userAge = ""
+
+ // Illegal variable _____________
+ 
+//  var 123 (Numbers)
+
+// var user-name (hyphen)
+
+// var if , statement (reserved keyword)
+
+// var abc@efg (symbol)
+
+// var abc ced (space)
+
+
+
+// Q3 (a)
+
+document.write("<h1> Rules for naming JS variables </h1>")
+
+// Q3 (b)
+
+document.write(" Variables names can only contain Letters , numbers , dollor signs , and underscores")
+
+
+// Q3 (c)
+
+document.write(" <br> Variables must begin with a letter, underscore (_), or dollar sign ($).") 
+
+// Q3 (d)
+
+document.write("<br> Variable names are case sensitive.") 
+
+// ________________________________-
+
+// Chap 5
+
+// Q1
+
+var number1 = 3
+var number2 = 5
+
+
+var calculation = (number1 + number2)
+console.log(calculation)
+
+// Q2
+
+var number1 = 3
+var number2 = 5
+
+// Subtraction
+var calculation = (number1 - number2)
+console.log(calculation)
+
+// Multiplication
+console.log(number1 * number2)
+
+// Division
+console.log(number1 / number2)
+
+// Modulus
+console.log(number1 % number2)
+
+// Q3
+
+// part (a)
+var myVariable;
+
+//  part (b)
+document.write(" <br> <br> Value after variable declaration is:" + myVariable)
+
+//  part (c)
+
+var myVariable = 5
+
+//  part (d)
+document.write(" <br> Initial value:" + myVariable)
+
+// part (e , f)
+var myVariable = 5
+
+document.write(" <br> Value after increment is:"  +  ++myVariable + "<br>")
+
+
+ // g. Add 7 to the variable.
+ myVariable += 7;
+
+ // h. Show the value of the variable after addition.
+ document.write("Value after addition is: " + myVariable + ".<br>");
+
+ // i. Decrement the variable.
+ myVariable--;
+
+ // j. Show the value of the variable after decrement.
+ document.write("Value after decrement is: " + myVariable + ".<br>");
+
+ // k. Show the remainder after dividing the variable's value by 3.
+ var remainder = myVariable % 3;
+
+ // l. Show the remainder in the browser.
+ document.write("The remainder is: " + remainder + ".");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
